c2ad5008e02dd455cf44ff110ee97f31ca9c82e0
Роман Майборода <mayboroda.ro@glonasssoft.ru>
2020-10-02 17:07:12 +0300

    Create User.cs

---
+++ MainService/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainService.Models
+{
+    internal class User
+    {
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+        internal string MiddleName { get; set; }
+        internal string PhoneNumber { get; set; }
+        internal string EMail { get; set; }
+
+    }
+}
